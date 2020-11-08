6d589e2c535023d58e1e7c0653187c851b5480eb
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-08 18:43:18 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   collections = []
   count = 0
   while collection.length > count
-    collections << (yield[count])
+    collections << yield(collection[count])
   
   count += 1
   end
